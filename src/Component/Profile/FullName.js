fecf09f34239d7115072b3316316e61400d8d723
Topsymac <temitopsy35@gmail.com>
2022-11-26 01:25:17 +0100

    first commit

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FullName = () => {
+  return (
+    <div>
+        <h1>Macaulay Temitope</h1>
+    </div>
+  )
+}
+
+export default FullName
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Picture from '../../../src/MyPicture.jpeg'
+
+const ProfilePhoto = () => {
+  return (
+    <div>
+        <img src={Picture} alt="" />
+    </div>
+  )
+}
+
+export default ProfilePhoto
